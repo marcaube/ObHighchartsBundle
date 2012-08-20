fe2b5391e29c5d2a84f57a977e7ca0adcfb5be10
Marc Aubé <marcaube@Marc.local>
2012-08-20 15:44:32 -0400

    try to bootstrap symphony for travis

---
+++ Tests/bootstrap.php
@@ -0,0 +1,19 @@
+<?php
+
+if (file_exists($file = __DIR__.'/../vendor/autoload.php')) {
+    $autoload = require_once $file;
+} else {
+    throw new RuntimeException('Install dependencies to run test suite.');
+}
+
+spl_autoload_register(function($class) {
+    if (0 === strpos($class, 'Ob\\HighchartsBundle\\')) {
+        $path = __DIR__.'/../'.implode('/', array_slice(explode('\\', $class), 2)).'.php';
+        if (!stream_resolve_include_path($path)) {
+            return false;
+        }
+        require_once $path;
+
+        return true;
+    }
+});
