c7a9ae40fa75c8fb268416131297a13bc56a5b41
Sam Van der Borght <sam@king-foo.be>
2013-11-15 14:56:27 +0100

    Implement pane option, fix #26

---
+++ Tests/PaneTest.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace Ob\HighchartsBundle\Tests;
+
+use Ob\HighchartsBundle\Highcharts\Highchart;
+
+/**
+ * This class hold Unit tests for the pane option
+ */
+class PaneTest extends \PHPUnit_Framework_TestCase
+{
+    public function testBackground()
+    {
+        $this->markTestIncomplete(
+            'This test has not been implemented yet.'
+        );
+    }
+
+    public function testCenter()
+    {
+        $chart = new Highchart();
+
+        // pixel based
+        $chart->pane->center(array(50, 100));
+        $this->assertRegExp('/pane: \{"center":\[50,100\]\}/', $chart->render());
+
+        // percentage based
+        $chart->pane->center(array('50%', '40%'));
+        $this->assertRegExp('/pane: \{"center":\["50%","40%"\]\}/', $chart->render());
+    }
+
+    public function testEndAngle()
+    {
+        $chart = new Highchart();
+
+        $chart->pane->endAngle(5);
+        $this->assertRegExp('/pane: \{"endAngle":5\}/', $chart->render());
+    }
+
+    public function testStartAngle()
+    {
+        $chart = new Highchart();
+
+        $chart->pane->startAngle(5);
+        $this->assertRegExp('/pane: \{"startAngle":5\}/', $chart->render());
+    }
+}
