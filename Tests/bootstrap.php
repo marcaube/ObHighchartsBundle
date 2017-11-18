a542dd5d1b705dd2a924388ddbfa8c7fdf22ec92
Sébastien Lévêque <sleveque@emisys.eu>
2017-11-18 12:37:38 +0100

    Add support for PHPUnite <6 and 6 // PHPUnit 6 introduced a breaking change that // removed PHPUnit_Framework_TestCase as a base class, // and replaced it with \PHPUnit\Framework\TestCase

--- Tests/bootstrap.php
+++ Tests/bootstrap.php
@@ -1,9 +1,9 @@
 <?php
 // PHPUnit 6 introduced a breaking change that
 // removed PHPUnit_Framework_TestCase as a base class,
-// and replaced it with \PHPUnit\Framework\TestCase
-if (!class_exists('\PHPUnit_Framework_TestCase') && class_exists('\PHPUnit\Framework\TestCase'))
-    class_alias('\PHPUnit\Framework\TestCase', '\PHPUnit_Framework_TestCase');
+// and replaced it with PHPUnit\Framework\TestCase
+if (!class_exists('\PHPUnit_Framework_TestCase') && class_exists('PHPUnit\Framework\TestCase'))
+    class_alias('PHPUnit\Framework\TestCase', '\PHPUnit_Framework_TestCase');
 
 if (!is_file($autoloadFile = __DIR__ . '/../vendor/autoload.php') &&
     !is_file($autoloadFile = __DIR__ . '/../../../../../autoload.php')) {
